80d933bf8049d52a672ef715c89f0ba5090c6cfd
andrey <btk.st@yandex.ru>
2021-09-15 19:53:27 +0300

    add 2 after hello world

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("A");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world2!");
 	System.out.println("B");
     }
 }
